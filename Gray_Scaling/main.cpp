eb86c357dcb68e9acfde78bb261114b1398cf413
KinoshitaYosuke <k1410159@edu.cc.uec.ac.jp>
2018-01-20 12:39:28 +0900

    asd

--- Gray_Scaling/main.cpp
+++ Gray_Scaling/main.cpp
@@ -119,11 +119,11 @@ void RGB_to_Gray() {
 	//テスト画像ファイル一覧メモ帳読み込み
 	char test_name[1024], test_name2[1024];
 	FILE *test_data, *test_data2;
-	fopen_s(&test_data, "C:/photo/train_data_from_demo/pre_experiment_data/Arbitrary_Poses/list_lie.txt", "r");
+	fopen_s(&test_data, "list_cd_2.txt", "r");
 
 	while (fgets(test_name, 256, test_data) != NULL) {
 
-		cout << test_name << endl;
+		
 
 		string name_tes = test_name;
 		char new_test_name[1024];
@@ -132,17 +132,27 @@ void RGB_to_Gray() {
 			new_test_name[i + 1] = '\0';
 		}
 
+		char before_name[1024] = "CD_pos/";
+		strcat_s(before_name, new_test_name);
+
+	//	cout << before_name << endl;
 
 		//画像の取り込み
-		cv::Mat img = cv::imread(new_test_name, 1);	//検出する画像
-//		img = img(cv::Rect(0, img.rows / 4, img.cols, img.rows / 2));
+		cv::Mat img = cv::imread(before_name, 1);	//検出する画像
+		if (img.empty() == true) { 
+			cout << before_name << endl;
+			continue; 
+		}
 //		cv::imshow("", img);
 //		cvWaitKey(10);
-		if (img.channels() == 1) cv::imwrite(new_test_name, img);
+
+		char after_name[1024]="after/";
+		strcat_s(after_name, new_test_name);
+		if (img.channels() == 1) cv::imwrite(after_name, img);
 		else {
 			cv::Mat gray;
 			cv::cvtColor(img, gray, CV_RGB2GRAY);
-			cv::imwrite(new_test_name, gray);
+			cv::imwrite(after_name, gray);
 		}
 
 	}
@@ -179,6 +189,99 @@ void Normalize_One_Pose() {
 			cv::imwrite(new_test_name, gray);
 		}
 
+	}
+	fclose(test_data);
+}
+
+void Normalize_INRIA() {
+	//テスト画像ファイル一覧メモ帳読み込み
+	char test_name[1024], test_name2[1024];
+	FILE *test_data, *test_data2;
+	fopen_s(&test_data, "INRIA-list.txt", "r");
+
+	while (fgets(test_name, 256, test_data) != NULL) {
+
+		cout << test_name << endl;
+
+		string name_tes = test_name;
+		char new_test_name[1024];
+		for (int i = 0; i < name_tes.length() - 1; i++) {
+			new_test_name[i] = test_name[i];
+			new_test_name[i + 1] = '\0';
+		}
+
+		char after_name[1024];
+		for (int i = 0; i < name_tes.length() - 1; i++) {
+			after_name[i] = new_test_name[i];
+			if (new_test_name[i] == '.') {
+				after_name[i + 1] = 'b';
+				after_name[i + 2] = 'm';
+				after_name[i + 3] = 'p';
+				after_name[i + 4] = '\0';
+				break;
+			}
+
+		}
+
+		//画像の取り込み
+		cv::Mat img = cv::imread(new_test_name, 1);	//検出する画像
+		img = img(cv::Rect(img.cols / 2 - 32 , img.rows / 2 - 64, 64, 128));
+		cv::imshow("", img);
+		cvWaitKey(10);
+		if (img.channels() == 1) cv::imwrite(after_name, img);
+		else {
+			cv::Mat gray;
+			cv::cvtColor(img, gray, CV_RGB2GRAY);
+			cv::imwrite(after_name, gray);
+		}
+
+	}
+	fclose(test_data);
+}
+
+void Gray_Test_INRIA() {
+	//テスト画像ファイル一覧メモ帳読み込み
+	char test_name[1024], test_name2[1024];
+	FILE *test_data, *test_data2;
+	fopen_s(&test_data, "INRIA-test-list.txt", "r");
+
+	while (fgets(test_name, 256, test_data) != NULL) {
+
+		cout << test_name << endl;
+
+		string name_tes = test_name;
+		char new_test_name[1024];
+		for (int i = 0; i < name_tes.length() - 1; i++) {
+			new_test_name[i] = test_name[i];
+			new_test_name[i + 1] = '\0';
+		}
+
+		char after_name[1024];
+		for (int i = 0; i < name_tes.length() - 1; i++) {
+			after_name[i] = new_test_name[i];
+			if (new_test_name[i] == '.') {
+				after_name[i + 1] = 'b';
+				after_name[i + 2] = 'm';
+				after_name[i + 3] = 'p';
+				after_name[i + 4] = '\0';
+				break;
+			}
+
+		}
+
+		//画像の取り込み
+		cv::Mat img = cv::imread(new_test_name, 1);	//検出する画像
+		
+		cv::imshow("", img);
+		cvWaitKey(10);
+		if (img.cols == 640 && img.rows == 480) {
+			if (img.channels() == 1) cv::imwrite(after_name, img);
+			else {
+				cv::Mat gray;
+				cv::cvtColor(img, gray, CV_RGB2GRAY);
+				cv::imwrite(after_name, gray);
+			}
+		}
 	}
 	fclose(test_data);
 }
@@ -325,7 +428,7 @@ public:
 };
 
 cv::Mat draw_rectangle(cv::Mat ans_im, int x, int y, int width, int height, int r, int g, int b) {
-	rectangle(ans_im, cvPoint(x, y), cvPoint(x + width, y + height), CV_RGB(r, g, b), 1);
+	rectangle(ans_im, cvPoint(x, y), cvPoint(x + width, y + height), CV_RGB(r, g, b), 2);
 	return ans_im;
 }
 
@@ -337,7 +440,7 @@ void Result_MR_and_FPPI() {
 	//テキストファイルのリスト読み込み
 	char List_n[1024];
 	FILE *List;
-	if (fopen_s(&List, "c:/photo/text_list.txt", "r") != 0) {
+	if (fopen_s(&List, "c:/photo/text_list_4.txt", "r") != 0) {
 		cout << "not found List file" << endl;
 		return;
 	}
@@ -371,7 +474,7 @@ void Result_MR_and_FPPI() {
 			num_G++;
 		}
 		fclose(GT);
-		char Result_name[1024] = "C:/photo/result_data_from_demo/2018_01_04_EP_50/result_data/";
+		char Result_name[1024] = "C:/photo/result_data_from_demo/2018_01_13_OOP/save_data/0.9_100/";
 		strcat_s(Result_name, List_name);
 		//Resultファイル読み込み
 		char Result_n[5][1024];
@@ -380,7 +483,7 @@ void Result_MR_and_FPPI() {
 			cout << "not found Result file" << endl;
 			return;
 		}
-		Place place_Result[10];
+		Place place_Result[1000];
 		int num_R = 0;
 		while (fgets(Result_n[0], 256, Result) != NULL) {	//すべて読み込み，変数に格納
 			fgets(Result_n[1], 256, Result);
@@ -409,8 +512,8 @@ void Result_MR_and_FPPI() {
 		}
 		char Img_name[1024]="C:/photo/test_data_from_demo/test_data/";
 		strcat_s(Img_name, Img_n);
-	//	cout << Img_name << endl;
-		cout << num_G << endl;
+		cout << Img_name << endl;
+	//	cout << num_G << endl;
 		cv::Mat img = cv::imread(Img_name, 1);
 
 		for (int i = 0; i < num_G; i++) {
@@ -435,7 +538,7 @@ void Result_MR_and_FPPI_2() {
 	//テキストファイルのリスト読み込み
 	char List_n[1024];
 	FILE *List;
-	if (fopen_s(&List, "c:/photo/text_list.txt", "r") != 0) {
+	if (fopen_s(&List, "c:/photo/text_list_4.txt", "r") != 0) {
 		cout << "not found List file" << endl;
 		return;
 	}
@@ -473,7 +576,7 @@ void Result_MR_and_FPPI_2() {
 			num_G++;
 		}
 		fclose(GT);
-		char Result_name[1024] = "C:/photo/result_data_from_demo/2018_01_04_AP/result_data/";
+		char Result_name[1024] = "C:/photo/result_data_from_demo/2018_01_15_AP/save_data/0.9_100/";
 		strcat_s(Result_name, List_name);
 		//Resultファイル読み込み
 		char Result_n[10][1024];
@@ -482,7 +585,7 @@ void Result_MR_and_FPPI_2() {
 			cout << "not found Result file" << endl;
 			return;
 		}
-		Place place_Result[2][10];
+		Place place_Result[2][1000];
 		int num_R = 0;
 		while (fgets(Result_n[0], 256, Result) != NULL) {	//すべて読み込み，変数に格納
 			fgets(Result_n[1], 256, Result);
@@ -536,8 +639,9 @@ void Result_MR_and_FPPI_2() {
 				img = draw_rectangle(img, place_Result[0][i].x, place_Result[0][i].y, place_Result[0][i].width, place_Result[0][i].height, 255, 0, 0);
 		}
 		for (int i = 0; i < num_R; i++) {
-			if (place_Result[1][i].yudo >= 0.995)
+			if (place_Result[1][i].yudo >= 0.5) {
 				img = draw_rectangle(img, place_Result[1][i].x, place_Result[1][i].y, place_Result[1][i].width, place_Result[1][i].height, 0, 255, 0);
+			}
 		}
 		cv::imshow("", img);
 		cvWaitKey(0);
@@ -554,6 +658,10 @@ int main(int argc, char** argv) {
 //	RGB_to_Gray();
 
 //	Normalize_One_Pose();
+
+//	Normalize_INRIA();
+
+//	Gray_Test_INRIA();
 
 //	check();
 
